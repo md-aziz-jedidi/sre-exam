82276fe01587f5add1a8537ca31ce45f52aff909
aziz jedidi <azizjedidi@takiacademyteam.com>
2024-02-23 17:45:36 +0100

    ok

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 import requests
+from prometheus_client import Histogram, generate_latest, CONTENT_TYPE_LATEST
+import time
+
+SEARCH_REQUEST_DURATION_SECONDS = Histogram(
+    "search_request_duration_seconds",
+    "Duration of /search endpoint",
+    ["endpoint"]
+)
+
+def track_search_request_duration(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        duration = time.time() - start_time
+        SEARCH_REQUEST_DURATION_SECONDS.labels(endpoint="/search").observe(duration)
+        return result
+    return wrapper
 
 app = Flask(__name__)
 
@@ -9,8 +26,8 @@ def home():
 
     return render_template("home.html")
 
-
 @app.route("/search", methods=["POST"])
+@track_search_request_duration
 def search():
 
     # Get the search query
@@ -37,3 +54,7 @@ def search():
     else:
 
         return render_template("fail.html")
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
